96c246a908f4191f7ffcbe27cd5ea77d8b987f33
PascalTUBerlin <Pascal.Meissner@mailbox.tu-berlin.de>
2019-05-08 09:38:43 +0200

    added Shapefilter to CreateNetworkFromOSM

--- src/main/java/org/matsim/class2019/network/CreateNetworkFromOSM.java
+++ src/main/java/org/matsim/class2019/network/CreateNetworkFromOSM.java
@@ -2,18 +2,27 @@ package org.matsim.class2019.network;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.network.NetworkWriter;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.network.algorithms.NetworkCleaner;
 import org.matsim.core.utils.geometry.CoordinateTransformation;
+import org.matsim.core.utils.geometry.geotools.MGC;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
+import org.matsim.core.utils.gis.ShapeFileReader;
 import org.matsim.core.utils.io.OsmNetworkReader;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.geometry.TransfiniteSet;
+import org.locationtech.jts.geom.Geometry;
 
 public class CreateNetworkFromOSM {
 	
 	private static Path inputFile = Paths.get("C:\\Users\\Pascal\\Desktop\\Multi agent transport simulation\\uebung02\\thueringen-latest.osm");
+	private static Path filterShape = Paths.get("C:\\Users\\Pascal\\Desktop\\Multi agent transport simulation\\uebung02\\erfurt-shape\\erfurt.shp");
 	private static String epsg = "EPSG:25832";
 	
 	public static void main (String[] args) {
@@ -36,14 +45,44 @@ public class CreateNetworkFromOSM {
 			return hierarchy <=4;
 		});
 		
+		reader.addOsmFilter(new NetworkFilter(filterShape));
+		
 		reader.parse(inputFile.toString());
 		
 		new NetworkCleaner().run(network);
 		
-		new NetworkWriter(network).write(Paths.get("C:\\Users\\Pascal\\Desktop\\Multi agent transport simulation\\uebung02\\test-network.xml.gz").toString());
+		new NetworkWriter(network).write(Paths.get("C:\\Users\\Pascal\\Desktop\\Multi agent transport simulation\\uebung02\\test-networkWithShapeFilter.xml.gz").toString());
 		
 				
 		
+	}
+	
+	/**
+	 * Includes motorways and primary roads for the whole dataset. If roads are contained within the supplied shape, also
+	 * secondary and residential roads are included
+	 */
+	private static class NetworkFilter implements OsmNetworkReader.OsmFilter {
+
+		private final Collection<Geometry> geometries = new ArrayList<>();
+
+		NetworkFilter(Path shapeFile) {
+			for (SimpleFeature feature : ShapeFileReader.getAllFeatures(shapeFile.toString())) {
+				geometries.add((Geometry) feature.getDefaultGeometry());
+			}
+		}
+
+		@Override
+		public boolean coordInFilter(Coord coord, int hierarchyLevel) {
+			// hierachy levels 1 - 3 are motorways and primary roads, as well as their trunks
+			if (hierarchyLevel <= 4) return true;
+
+			// if coord is within the supplied shape use every street above level of tracks and cycle ways
+			return hierarchyLevel <= 8 && containsCoord(coord);
+		}
+
+		private boolean containsCoord(Coord coord) {
+			return geometries.stream().anyMatch(geom -> geom.contains(MGC.coord2Point(coord)));
+		}
 	}
 	
 	
