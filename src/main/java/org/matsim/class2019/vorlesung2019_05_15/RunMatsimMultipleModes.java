4d720cf1f6ac3ec4d1f33ba6cbf1579caee44b40
PascalTUBerlin <Pascal.Meissner@mailbox.tu-berlin.de>
2019-05-22 11:28:13 +0200

    added scooter mode

--- src/main/java/org/matsim/class2019/vorlesung2019_05_15/RunMatsimMultipleModes.java
+++ src/main/java/org/matsim/class2019/vorlesung2019_05_15/RunMatsimMultipleModes.java
@@ -3,11 +3,14 @@ package org.matsim.class2019.vorlesung2019_05_15;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup.ModeParams;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup.ModeRoutingParams;
 import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.config.groups.StrategyConfigGroup.StrategySettings;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
 import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule;
+import org.matsim.core.replanning.strategies.DefaultPlanStrategiesModule.DefaultStrategy;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.scenario.ScenarioUtils;
 
@@ -23,18 +26,34 @@ public class RunMatsimMultipleModes {
 		config.controler().setOutputDirectory("./output");
 		OverwriteFileSetting  abc  = OverwriteFileSetting.deleteDirectoryIfExists;
 		config.controler().setOverwriteFileSetting( abc );
-		config.controler().setLastIteration(1);
+		config.controler().setLastIteration(10);
 		
+		{
 		StrategyConfigGroup.StrategySettings stratSets = new StrategyConfigGroup.StrategySettings();
 		stratSets.setStrategyName(DefaultPlanStrategiesModule.DefaultStrategy.ChangeSingleTripMode);
-		double probability = 0.1;
+		double probability = 1.0;
 		stratSets.setWeight(probability);
 		config.strategy().addStrategySettings(stratSets);
-		config.plansCalcRoute();
+//		config.plansCalcRoute();
 		
 		String[] modes = {"car","scooter"};
 		config.changeMode().setModes(modes);
+		}	
 		
+		{
+		ModeRoutingParams pars = new ModeRoutingParams();
+		String mode = "scooter";
+		pars.setMode(mode);
+		Double teleportedModeFreespeedFactor = 1.;
+		pars.setTeleportedModeFreespeedFactor(teleportedModeFreespeedFactor);
+		config.plansCalcRoute().addModeRoutingParams(pars );
+		}
+		
+		{
+			ModeParams params = new ModeParams("scooter");
+			params.setMarginalUtilityOfTraveling(0.);
+			config.planCalcScore().addModeParams(params);
+		}
 		
 		
 		
